12a69e2bf8e34eb4b7dd8b8f72d7f05d025e5e1f
Felippe <felippe.leme@gmail.com>
2020-03-17 23:09:59 -0300

    Payment, one-to-one association

--- src/main/java/com/flash/course/config/TestConfig.java
+++ src/main/java/com/flash/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.flash.course.entities.Category;
 import com.flash.course.entities.Order;
 import com.flash.course.entities.OrderItem;
+import com.flash.course.entities.Payment;
 import com.flash.course.entities.Product;
 import com.flash.course.entities.User;
 import com.flash.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPatment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
